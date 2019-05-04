5c5adc8152532e2b705c0855befaced61a60a02a
Ian Bryant <48700036+IXLives@users.noreply.github.com>
2019-05-04 13:10:32 -0400

    PreventDefault, MouseOver, Keydown, Wheel. Drag next, woof

--- js/index.js
+++ js/index.js
@@ -1,1 +1,51 @@
 // Your code goes here
+
+//NavBar prevent default
+const navBar = document.querySelectorAll('a');
+console.log(navBar);
+
+navBar.forEach(function(i){
+    i.addEventListener('click', function(e){
+        e.preventDefault();
+        console.log(`${e} prevented.`);
+    })
+})
+
+//MouseOver
+const navHeader = document.querySelector('.nav-container');
+//console.log(navHeader);
+
+navHeader.addEventListener('mouseover', function(e) {
+    e.target.style.backgroundColor = 'red';
+    console.log(e);
+});
+
+
+//KeyLogger (keydown)
+let keysLogged = '';
+document.addEventListener('keydown', function(e) {
+    keysLogged += e.key;
+    console.log(keysLogged);
+});
+
+//wheel zoom
+function zoom(e) {
+    e.preventDefault();
+
+    if(e.deltaY < 0) {
+        scale *= event.deltaY * -2;
+    } else {
+        scale /= event.deltaY * 2;
+    }
+
+    scale = Math.min(Math.max(.125, scale), 4);
+
+    headerImg.style.transform = `scale(${scale})`;
+}
+
+let scale = 1;
+const headerImg = document.querySelector('header img');
+headerImg.onwheel = zoom;
+
+//Drag/Drop
+
